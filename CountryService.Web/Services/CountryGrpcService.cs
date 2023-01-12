6d13341b9ef43f95cac490d077eaed8b2ea37406
Naman Singh <naman.singh@thedelta.io>
2023-01-12 08:49:56 +0200

    Add project files.

---
+++ CountryService.Web/Services/CountryGrpcService.cs
@@ -0,0 +1,36 @@
+﻿using Apress.Sample.gRPC;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using static Apress.Sample.gRPC.CountryService;
+
+namespace CountryService.Web.Services
+{
+    public class CountryGrpcService : CountryServiceBase
+    {
+        public override async Task GetAll(Empty request, IServerStreamWriter
+       <CountryReply> responseStream, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+        public override async Task<CountryReply> Get(CountryIdRequest request,
+       ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+        public override async Task<Empty> Delete(IAsyncStreamReader<CountryIdRequest> requestStream, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+        public override async Task<Empty> Update(CountryUpdateRequest request,
+       ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+        public override async Task Create(IAsyncStreamReader<CountryCreationRequest> requestStream, IServerStreamWriter<CountryCreationReply>
+responseStream, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+    }
+
+}
