6bd49b9ffd7e631563477dfdad78ff98982d523b
Naman Singh <naman.singh@thedelta.io>
2023-01-12 11:13:43 +0200

    Register and Apply gRPC Reflection

--- CountryService.Web/Program.cs
+++ CountryService.Web/Program.cs
@@ -12,10 +12,12 @@ builder.Services.AddGrpc(options =>
     options.MaxReceiveMessageSize = 6291456; // 6 MB
     options.MaxSendMessageSize = 6291456; // 6 MB
 });
+builder.Services.AddGrpcReflection();
 builder.Services.AddSingleton<CountryManagementService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcReflectionService();
 app.MapGrpcService<CountryGrpcService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
